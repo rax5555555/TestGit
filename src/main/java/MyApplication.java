2ee6c80b24aba025099762a2ffc09dc4b8ee0b49
Timur Rakhmatullin <rax_5@mail.ru>
2022-06-25 14:54:02 +0500

    add for

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,6 +1,8 @@
 public class MyApplication {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("Hello git");
+        for(int i = 0; i<5; i++) {
+            System.out.println(i);
+        }
     }
 }
