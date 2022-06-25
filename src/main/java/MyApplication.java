1c06e7e29fbf00f84530d82de1d1e71674343aa0
Timur Rakhmatullin <rax_5@mail.ru>
2022-06-25 16:52:21 +0500

    Relise 2.0

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -2,11 +2,20 @@ public class MyApplication {
 
     /**
      *
+     * @param count число
+     * @return возврат
+     */
+    public static int numberCount (int count) {
+        return count;
+    }
+
+    /**
+     *
      * @param args значение
      */
     public static void main(String[] args) {
         System.out.println("Hello git");
-        for(int i = 0; i<5; i++) {
+        for(int i = 0; i<numberCount(10); i++) {
             System.out.println(i);
         }
         System.out.println("End program");
